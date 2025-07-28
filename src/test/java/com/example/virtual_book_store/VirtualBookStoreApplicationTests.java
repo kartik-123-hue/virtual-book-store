5a27e7149cfeced9881576d1b4f831c38d37b59b
kartik mehta <kartikmehta@kartiks-MacBook-Air.local>
2025-07-29 00:18:08 +0530

    first commit

---
+++ src/test/java/com/example/virtual_book_store/VirtualBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtual_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
